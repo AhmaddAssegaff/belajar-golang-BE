2c51194ca8d2160c2dd3a17d8e1c6fdfbbb4454c
Ahmad <ahmadasgf89@gmail.com>
2026-04-19 15:09:25 +0700

    chore: move to simpler arch

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,9 @@ package main
 
 import (
 	"log/slog"
-	"net/http"
 
 	"belajar-go-be/config"
 	"database/sql"
-
-	productHttp "belajar-go-be/internal/product/adapter/http"
-	productRepo "belajar-go-be/internal/product/adapter/repository"
-	productApp "belajar-go-be/internal/product/application"
 
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
@@ -30,28 +25,12 @@ func main() {
 		return
 	}
 
-	// Echo instance
 	e := echo.New()
 
-	repo := productRepo.NewProductRepository(db)
-	service := productApp.NewProductService(repo)
-	handler := productHttp.NewProductHandler(service)
+	e.Use(middleware.RequestLogger())
+	e.Use(middleware.Recover())
 
-	// Middleware
-	e.Use(middleware.RequestLogger()) // use the RequestLogger middleware with slog logger
-	e.Use(middleware.Recover())       // recover panics as errors for proper error handling
-
-	// Routes
-	e.GET("/", hello)
-	e.GET("/products", handler.GetAll)
-
-	// Start server
 	if err := e.Start(cfg.AppHost); err != nil {
 		slog.Error("failed to start server", "error", err)
 	}
-}
-
-// Handler
-func hello(c *echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
 }
--- internal/product/domain/product_repository.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-import "context"
-
-type ProductRepository interface {
-	FindAll(ctx context.Context) ([]Product, error)
-}
